5b454133457571a7db0869dbb7708e357c0ae44d
mrmur <andreiduvakin@mail.ru>
2022-03-29 18:59:34 +0500

    Создана база данных, сделана таблица пользователей

---
+++ data/users.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from datetime import date
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    photo = sqlalchemy.Column(sqlalchemy.Text)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    data_reg = sqlalchemy.Column(sqlalchemy.Date,
+                                 default=date.today())
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'moona_secret_key'
 
 
-@app.route('/')
-def index():
-    return render_template('base.html')
-
-
-def run_web():
+def main():
+    db_session.global_init("db/moona_data.db")
     app.run()
 
 
 if __name__ == '__main__':
-    run_web()
+    main()
